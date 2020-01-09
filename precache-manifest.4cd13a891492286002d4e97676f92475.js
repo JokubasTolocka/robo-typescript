51c420c1397bbdbca6d00997a5d018594e571a4b
Jokūbas Toločka <jokubas.tj@gmail.com>
2020-01-09 17:28:23 +0200

    Updates

---
+++ precache-manifest.4cd13a891492286002d4e97676f92475.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b8659d08692085977206baa9d52ccb",
+    "url": "/robofriends-typescript/index.html"
+  },
+  {
+    "revision": "ead8b1d139e81f83f6c6",
+    "url": "/robofriends-typescript/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5ce8691c243d0b2ff8b8",
+    "url": "/robofriends-typescript/static/css/main.6463ac0e.chunk.css"
+  },
+  {
+    "revision": "ead8b1d139e81f83f6c6",
+    "url": "/robofriends-typescript/static/js/2.866dfc00.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends-typescript/static/js/2.866dfc00.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5ce8691c243d0b2ff8b8",
+    "url": "/robofriends-typescript/static/js/main.0c5d11f3.chunk.js"
+  },
+  {
+    "revision": "974a4b7247ddbdae037e",
+    "url": "/robofriends-typescript/static/js/runtime-main.8b857810.js"
+  }
+]);
